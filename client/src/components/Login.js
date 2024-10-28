aaa9e920168eea7ed69aa1ac84ec7a1d7ca945b8
Ojasv Dixit <115476649+Ojasvdixit@users.noreply.github.com>
2024-10-28 10:48:18 +0530

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
 
-const URL = "http://localhost:5000/api/auth/login";
+const URL = "https://textmania-mzuj.onrender.com";
 
 
 export default function Login(props) {
